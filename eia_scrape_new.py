5665083a6afbd75d7b3f875e0981cbdfe8ea1d4f
Alison Saldanha <alisonrsald@gmail.com>
2025-08-13 11:54:40 -0700

    Add files via upload

---
+++ eia_scrape_new.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.eia.gov/electricity/annual/html/epa_11_01.html"
+
+eia_table = pd.read_html(url)
+
+#print(eia_table)
+print(eia_table[1])
+#print(eia_table[0])
+
+eia_table[1].to_csv('/Users/alison.saldanha@dallasnews.com/code/lessons/sandbox1/first_scraper/new/eia_new.csv', index=False)
+
